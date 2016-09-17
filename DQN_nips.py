43740612027f86169f92bc742594466a8639b007
vidurj <vidur94@gmail.com>
2016-09-17 09:13:41 -0700

    before run commit

--- DQN_nips.py
+++ DQN_nips.py
@@ -4,14 +4,13 @@ import cv2
 
 
 class DQN:
-    def __init__(self, params, name):
+    def __init__(self, params, _):
         self.params = params
-        self.network_name = name
-        sel.x = tf.placeholder('float32', [None, 84, 84, 4], name=network_name + '_x')
-        self.q_t = tf.placeholder('float32', [None], name=network_name + '_q_t')
-        self.actions = tf.placeholder("float32", [None, params['num_act']], name=network_name + '_actions')
-        self.rewards = tf.placeholder("float32", [None], name=network_name + '_rewards')
-        self.terminals = tf.placeholder("float32", [None], name=network_name + '_terminals')
+        sel.x = tf.placeholder('float32', [None, 84, 84, 4])
+        self.q_t = tf.placeholder('float32', [None])
+        self.actions = tf.placeholder("float32", [None, params['num_act']])
+        self.rewards = tf.placeholder("float32", [None])
+        self.terminals = tf.placeholder("float32", [None])
 
         # conv1
         with tf.variable_scope("CONV"):
@@ -20,8 +19,8 @@ class DQN:
             channels = 4
             filters = 16
             stride = 4
-            w1 = tf.Variable(tf.random_normal([size, size, channels, filters], stddev=0.01))
-            b1 = tf.Variable(tf.constant(0.1, shape=[filters]))
+            self.w1 = tf.Variable(tf.random_normal([size, size, channels, filters], stddev=0.01))
+            self.b1 = tf.Variable(tf.constant(0.1, shape=[filters]))
             c1 = tf.nn.conv2d(x, w1, strides=[1, stride, stride, 1], padding='VALID')
             o1 = tf.nn.relu(tf.add(c1, b1))
 
@@ -30,8 +29,8 @@ class DQN:
             channels = 16
             filters = 32
             stride = 2
-            w2 = tf.Variable(tf.random_normal([size, size, channels, filters], stddev=0.01))
-            b2 = tf.Variable(tf.constant(0.1, shape=[filters]))
+            self.w2 = tf.Variable(tf.random_normal([size, size, channels, filters], stddev=0.01))
+            self.b2 = tf.Variable(tf.constant(0.1, shape=[filters]))
             c2 = tf.nn.conv2d(o1, w2, strides=[1, stride, stride, 1], padding='VALID')
             o2 = tf.nn.relu(tf.add(c2, b2))
 
@@ -43,17 +42,21 @@ class DQN:
             hiddens = 512
             dim = o2_shape[1] * o2_shape[2] * o2_shape[3]
             o2_flat = tf.reshape(o2, [-1, dim])
-            w3 = tf.Variable(tf.random_normal([dim, hiddens], stddev=0.01))
-            b3 = tf.Variable(tf.constant(0.1, shape=[hiddens]))
+            self.w3 = tf.Variable(tf.random_normal([dim, hiddens], stddev=0.01))
+            self.b3 = tf.Variable(tf.constant(0.1, shape=[hiddens]))
             self.mask = tf.placeholder(tf.float32, (None, 512))
             o3_dropout = tf.nn.relu(tf.add(tf.matmul(o2_flat, w3), b3)) * self.mask
 
             # fc4
             hiddens = params['num_act']
             dim = 512
-            w4 = tf.Variable(tf.random_normal([dim, hiddens], stddev=0.01))
-            b4 = tf.Variable(tf.constant(0.1, shape=[hiddens]))
+            self.w4 = tf.Variable(tf.random_normal([dim, hiddens], stddev=0.01))
+            self.b4 = tf.Variable(tf.constant(0.1, shape=[hiddens]))
             y = tf.nn.softplus(tf.add(tf.matmul(o3_dropout, w4), b4))
+
+        # dummy for nature network
+        self.w5 = tf.Variable(tf.constant(1.0))
+        self.b5 = tf.Variable(tf.constant(1.0))
 
         # Q,Cost,Optimizer
         discount = tf.constant(params['discount'])
@@ -67,8 +70,8 @@ class DQN:
         global_step = tf.Variable(0, name='global_step', trainable=False)
         optimizer = tf.train.RMSPropOptimizer(params['lr'], params['rms_decay'], 0.0,
                                               params['rms_eps'])
-        fc_variables = tf.get_collection(tf.GraphKeys.VARIABLES, scope='FC')
-        self.rmsprop = optimizer.minimize(cost, global_step=global_step, var_list=fc_variables)
+        #fc_variables = tf.get_collection(tf.GraphKeys.VARIABLES, scope='FC')
+        self.rmsprop = optimizer.minimize(cost, global_step=global_step)#, var_list=fc_variables)
 
         # masks = tf.Variable(np.zeros((50, 512)))
         # o3_reshaped = tf.reshape(o3, shape=[512])
